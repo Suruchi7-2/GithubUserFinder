0bc8379e890c2b16084f3ae8af49a53b35dff0e5
Suruchi7-2 <suruchikumari8579@gmail.com>
2024-03-15 04:58:03 +0530

    finally done userfetched

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <title>Github Finder</title>
   </head>
   <body>
-
+<!-- see -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
       <div class="container">
         <div class="navbar-header">
@@ -53,6 +53,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <!-- Optional JavaScript -->
-    <script src="./js/main.js"></script>
+    <script src="main.js"></script>
   </body>
 </html>
